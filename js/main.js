5be817f9f24d2c84042b7fe4d20a769cc96c5982
Calvin Hoenes <calvinhoenes@gmail.com>
2018-01-18 01:05:08 +0100

    Added roadmap, contact, team, footer. Changed styles. Added Mailchimp (Integration missing)

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
+        console.log("FUCKER");
         $('html, body').animate({
-          scrollTop: (target.offset().top)
+          scrollTop: (target.offset().top - 112)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -40,7 +41,7 @@
         $(el).addClass('in-view');
       })
       .on('exit', el => {
-          $(el).removeClass('in-view');
+          // $(el).removeClass('in-view');
           if($('.section.in-view').hasClass('section-white') || $('.section.in-view').hasClass('section-grey')){
             $("body").addClass('light-bg');
           }else{
@@ -50,5 +51,51 @@
     inView.offset(250);
 
   })
+
+  function register($form) {
+    $.ajax({
+      type: "GET",
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache       : false,
+      dataType    : 'jsonp',
+      contentType : "application/json; charset=utf-8",
+      beforeSend  : function() {
+  			$form.find('.alert').html('Subscribing...').show();
+  			$form.find('.email').hide();
+  			$form.find('.submit').hide();
+  			$form.find('.discount').hide();
+  		},
+      error       : function(err) {
+  			$form.find('.alert').html('Ops, there was an error.').show();
+  			$form.find('.email').show();
+  			$form.find('.submit').show();
+  			$form.find('.discont').show();
+      },
+      success     : function(data) {
+        if (data.result != "success") {
+          $form.find('.alert').html(data.msg).show();
+    			$form.find('.email').show();
+    			$form.find('.submit').show();
+    			$form.find('.discont').show();
+        } else {
+          $form.find('.alert').html('Thank you!').show();
+        }
+      }
+    });
+  }
+
+  // waits for form to appear rather than appending straight to the form. Also helps if you have more than one type of form that you want to use this action on.
+  $(document).on('submit', '#mc-embedded-subscribe-form', function(event) {
+      try {
+        //define argument as the current form especially if you have more than one
+        var $form = $(this);
+        // stop open of new tab
+        event.preventDefault();
+        // submit form via ajax
+        register($form);
+      } catch(error){}
+    });
+
 
 })(jQuery)
