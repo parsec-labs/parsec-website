b8525886dab919d4255990e5b4c713925a674c43
Alex Lunyov <isuntc@gmail.com>
2018-03-15 12:35:06 +0800

    Maked promoCode input

--- src/js/presale/main.js
+++ src/js/presale/main.js
@@ -1,6 +1,12 @@
 (() => {
   const promoForm = document.querySelector('.promo-form');
   const submitButton = promoForm.querySelector('.button[type=submit]');
+  const codeInput = promoForm.querySelector('.input[name=promoCode]');
+
+  new IMask(codeInput, {
+    mask: '0000-0000-0000',
+  });
+
   promoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     if (promoForm.querySelector('.row[data-error]')) {
@@ -18,6 +24,7 @@
         document.querySelector('.promo-form__content').dataset.mode = 'timer';
         document.querySelector('.intro p').innerHTML = `<strong>Thank you!</strong><br /> Some really motivational text goes here`;
         window.launchRocket();
+        submitButton.removeAttribute('disabled');
       },
       ({ errors }) => {
         Object.keys(errors).forEach((key) => {
@@ -28,13 +35,12 @@
         })
         document.querySelector('.rocket-status--fail').classList.add('st-active');
       }
-    ).then(() => {
-      submitButton.removeAttribute('disabled');
-    });
+    );
   });
 
   promoForm.addEventListener('change', () => {
     document.querySelector('.rocket-status--fail').classList.remove('st-active');
+    submitButton.removeAttribute('disabled');
     Array.from(promoForm.querySelectorAll('.row')).forEach((row) => {
       delete row.dataset.error;
     });
