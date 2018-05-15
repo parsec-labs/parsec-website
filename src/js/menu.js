53c44b888705078c300a18d0174e7f00a355beba
Max Kudla <maksim.kudla@gmail.com>
2018-05-15 12:10:08 +0300

    - updated footer section background - updated team section background - updated partners logo position - updated menu - fixed menu auto-hiding bug
    (cherry picked from commit 0471b8f)

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,26 @@
 (() => {
   const menuButton = document.querySelector('.top-header-menu button');
   const menuList = document.querySelector('.top-header-menu ul');
+  const header = document.querySelector('.top-header');
+  const sections = document.querySelector('.sections');
+
+
   if (menuButton && menuList) {
-    menuButton.addEventListener('click', () => {
+    menuButton.addEventListener('click', (e) => {
+      e.stopPropagation();
       menuList.classList.toggle('st-opened');
+    });
+
+    menuList.addEventListener('click', (e) => {
+      menuList.classList.remove('st-opened');
+    });
+
+    header.addEventListener('click', (e) => {
+      menuList.classList.remove('st-opened');
+    });
+
+    sections.addEventListener('click', (e) => {
+      menuList.classList.remove('st-opened');
     });
   }
 })();
